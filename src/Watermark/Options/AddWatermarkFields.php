511219dfd96c2285a863705a86ff44e6a0cc24bb
Jake Jackson <jake@gravitypdf.com>
2020-02-17 11:55:40 +1100

    Update copyright and remove verbose license

--- src/Watermark/Options/AddWatermarkFields.php
+++ src/Watermark/Options/AddWatermarkFields.php
@@ -8,7 +8,7 @@ use Monolog\Logger;
 
 /**
  * @package     Gravity PDF Watermark
- * @copyright   Copyright (c) 2019, Blue Liquid Designs
+ * @copyright   Copyright (c) 2020, Blue Liquid Designs
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
  */
@@ -17,26 +17,6 @@ use Monolog\Logger;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-/*
-	This file is part of Gravity PDF Watermark.
-
-	Copyright (c) 2019, Blue Liquid Designs
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
 
 /**
  * Class AddWatermarkFields
